197a56728fab74def3281d820150f45f61fc36d0
Alaa-Mustafaa <alaamustafaelsayedd@gmail.com>
2026-09-25 11:36:47 +0300

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./layouts/Main/main-layout/main-layout').then((m) => m.MainLayout),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./features/home/home').then((m) => m.Home),
+      },
+    ],
+  },
+];
